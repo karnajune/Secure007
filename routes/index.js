a1be4255eeedf3061ded3b30f56fd263ac165d2b
karnajune <karnajune@yahoo.com>
2017-11-16 14:54:50 +0000

    initial design

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Test MeanStack App' });
+    res.render('index', { title: 'Index Page' });
 });
 
 module.exports = router;
